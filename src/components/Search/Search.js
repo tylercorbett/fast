644dfc6b4ab9bea01fd151a9e7800091ff26de58
Tyler Corbett <tyler.corbett@nike.com>
2020-12-30 14:02:22 -0800

    handle serach clicked added

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,7 +3,11 @@ import './Search.scss';
 
 const Search = () => {
   const [movieName, setMovieName] = useState('');
-  
+
+  const handleSearchClicked = () => {
+    console.log(movieName)
+  }
+   
   return (
     <section className='Search'>
       <h2>Search by movie or series title</h2>
@@ -13,7 +17,9 @@ const Search = () => {
         onChange={e => setMovieName(e.target.value)}
         maxLength={40}
       />
-
+      <button
+        onClick={handleSearchClicked}
+      >Search</button>
     </section>
   );
 };
