3d45fb25f59b0b8ea69c4ba1beb9322f9a213117
Tyler Corbett <tyler.corbett@nike.com>
2020-12-30 14:07:56 -0800

    i'm getting search results into state

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,10 +3,22 @@ import './Search.scss';
 
 const Search = () => {
   const [movieName, setMovieName] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSearchClicked = () => {
-    console.log(movieName)
-  }
+  const handleSearchClicked = async () => {
+    setIsLoading(true);
+    const res = await fetch('http://www.omdbapi.com/?apikey=93675976&s=citizen%20kane');
+    console.log(res, 'res');
+
+    if (res.status === 200) {
+      const json = await res.json();
+      setSearchResults(json.Search);
+      console.log(json, 'json');
+    }
+
+    setIsLoading(false);    
+  };
    
   return (
     <section className='Search'>
