86de13e43ac92e9a2bb0e77db345eff8083374a2
Tyler Corbett <tyler.corbett@fast.co>
2021-12-04 18:43:38 -0800

    mostly full screen

--- src/components/TinderSwipe/TinderSwipe.js
+++ src/components/TinderSwipe/TinderSwipe.js
@@ -4,42 +4,42 @@ import TinderCard from "react-tinder-card";
 
 const db = [
   {
-    name: "Offroading",
+    name: "Offroading - July 31st 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669988/skye%2Btyler/offroading.gif",
     isVertical: true,
   },
   {
-    name: "Vegas pool party",
+    name: "Vegas pool party - August 28th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669988/skye%2Btyler/vegas-pool.gif",
     isVertical: true,
   },
   {
-    name: "Maya pool",
+    name: "Maya pool - July 11th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669986/skye%2Btyler/maya-pool.gif",
     isVertical: true,
   },
   {
-    name: "Vegas",
+    name: "Vegas - August 27th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669983/skye%2Btyler/vegas.gif",
     isVertical: true,
   },
   {
-    name: "Duck pond",
+    name: "Duck pond - July 14th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669982/skye%2Btyler/duck-pond.gif",
     isVertical: false,
   },
   {
-    name: "Mansion party",
+    name: "Mansion party - July 28th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669978/skye%2Btyler/mansion.gif",
     isVertical: true,
   },
   {
-    name: "Club",
+    name: "Club - July 14th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669978/skye%2Btyler/club.gif",
     isVertical: false,
   },
   {
-    name: "Heliopter ride",
+    name: "Heliopter ride - August 28th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669976/skye%2Btyler/hellacopters.gif",
     isVertical: true,
   },
