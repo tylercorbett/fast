0ac84d07f5a42a146a2228404a1e10a8a69a050d
Tyler Corbett <tyler.corbett@fast.co>
2021-12-04 18:15:20 -0800

    isvertical added

--- src/components/TinderSwipe/TinderSwipe.js
+++ src/components/TinderSwipe/TinderSwipe.js
@@ -6,34 +6,42 @@ const db = [
   {
     name: "Offroading",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669988/skye%2Btyler/offroading.gif",
+    isVertical: true,
   },
   {
     name: "Vegas pool party",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669988/skye%2Btyler/vegas-pool.gif",
+    isVertical: true,
   },
   {
     name: "Maya pool",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669986/skye%2Btyler/maya-pool.gif",
+    isVertical: true,
   },
   {
     name: "Vegas",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669983/skye%2Btyler/vegas.gif",
+    isVertical: true,
   },
   {
     name: "Duck pond",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669982/skye%2Btyler/duck-pond.gif",
+    isVertical: false,
   },
   {
     name: "Mansion party",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669978/skye%2Btyler/mansion.gif",
+    isVertical: true,
   },
   {
     name: "Club",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669978/skye%2Btyler/club.gif",
+    isVertical: false,
   },
   {
     name: "Heliopter ride",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669976/skye%2Btyler/hellacopters.gif",
+    isVertical: true,
   },
 ];
 
