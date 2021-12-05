4cf2f8f21f5142e06e21eec520a44d2de3aaa7c1
Tyler Corbett <tyler.corbett@fast.co>
2021-12-04 16:33:48 -0800

    auto deploy

--- src/App.js
+++ src/App.js
@@ -1,70 +1,15 @@
-import React, { useState } from 'react';
-import './reset.css';
-import './App.scss';
-import Search from './components/Search/Search';
-import Review from './components/Review/Review';
-import { FiSearch as SearchIcon } from 'react-icons/fi';
-import { GrCart as CartIcon } from 'react-icons/gr';
+import React from "react";
+import "./reset.css";
+import "./App.scss";
 
 const App = () => {
-  const [selectedMovies, setSelectedMovies] = useState([]);
-  const [isReviewVisible, setIsReviewVisible] = useState(false);
-
-  const updateSelectedMovies = newMovie => {
-    let newMovies = [...selectedMovies];
-    
-    if (newMovies.includes(newMovie)) {
-      newMovies = newMovies.filter(movie => movie !== newMovie);
-    }
-    else {
-      newMovies.push(newMovie);
-    }
-
-    setSelectedMovies(newMovies);
-  };
   return (
     <div className="App">
       <header>
-        <h1>
-          Fast Movie Search NEW NEW
-        </h1>
+        <h1>auto deploy go</h1>
       </header>
-      <Navigation 
-        setIsReviewVisible={setIsReviewVisible}
-        isReviewVisible={isReviewVisible}
-      />
-      <Search 
-        updateSelectedMovies={updateSelectedMovies}
-        selectedMovies={selectedMovies}
-      />
-     {isReviewVisible && 
-     <Review 
-        selectedMovies={selectedMovies}
-        setIsReviewVisible={setIsReviewVisible}
-        setSelectedMovies={setSelectedMovies}
-      />}
     </div>
   );
-}
-
+};
 
 export default App;
-
-const Navigation = ({ setIsReviewVisible, isReviewVisible }) => {
-  return (
-    <nav className='Navigation'>
-      <ul>
-        <li>
-          <button onClick={() => setIsReviewVisible(false)} className={isReviewVisible ? '' : 'active'}>
-            <SearchIcon />
-          </button>
-        </li>
-        <li>
-          <button onClick={() => setIsReviewVisible(true)} className={isReviewVisible ? 'active' : ''}>
-            <CartIcon />
-          </button>
-        </li>
-      </ul>
-    </nav>
-  );
-}
