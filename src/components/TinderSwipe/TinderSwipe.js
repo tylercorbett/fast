4f0178c03d0c9d483765c48756d0bb1845469b31
Tyler Corbett <tyler.corbett@fast.co>
2021-12-04 20:43:33 -0800

    add more pictures

--- src/components/TinderSwipe/TinderSwipe.js
+++ src/components/TinderSwipe/TinderSwipe.js
@@ -6,47 +6,54 @@ const db = [
   {
     name: "And more to come!",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638676652/skye%2Btyler/bxvnc3rxwz7ovwgd2b7d.jpg",
-    isVertical: true,
   },
   {
     name: "Offroading - July 31st 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669988/skye%2Btyler/offroading.gif",
-    isVertical: true,
   },
   {
     name: "Vegas Pool Party - August 28th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669988/skye%2Btyler/vegas-pool.gif",
-    isVertical: true,
+  },
+  {
+    name: "Birding - Second Day We Met",
+    src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638678928/skye%2Btyler/bird.gif",
   },
   {
     name: "Maya Pool - July 11th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669986/skye%2Btyler/maya-pool.gif",
-    isVertical: true,
   },
   {
     name: "Vegas - August 27th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669983/skye%2Btyler/vegas.gif",
-    isVertical: true,
   },
   {
     name: "Duck Pond - July 14th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669982/skye%2Btyler/duck-pond.gif",
-    isVertical: false,
   },
   {
     name: "Mansion Party - July 28th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669978/skye%2Btyler/mansion.gif",
-    isVertical: true,
   },
   {
     name: "Club - July 14th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669978/skye%2Btyler/club.gif",
-    isVertical: false,
   },
   {
     name: "Heliopter Ride - August 28th 2021",
     src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638669976/skye%2Btyler/hellacopters.gif",
-    isVertical: true,
+  },
+  {
+    name: "Talking Stick Casino - August 5th 2021",
+    src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638678439/skye%2Btyler/talking-stick.gif",
+  },
+  {
+    name: "Riding Back Home - First Day We Met",
+    src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638678929/skye%2Btyler/compressed-golf-cart.gif",
+  },
+  {
+    name: "Bellagio - August 29th 2021",
+    src: "https://res.cloudinary.com/dmtn5fbdu/image/upload/v1638678438/skye%2Btyler/sunglasses.gif",
   },
 ];
 
