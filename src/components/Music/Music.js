da142630446b8041f40028a855c36c158d11962d
Tyler Corbett <tyler.corbett@fast.co>
2021-12-09 19:07:38 -0800

    force the svg

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -17,11 +17,25 @@ const Music = () => {
     }
   };
 
-  const MusicIcon = isPlaying ? <MusicNote /> : <MusicNote className="off" />;
+  // const MusicIcon = isPlaying ? <MusicNote /> : <MusicNote className="off" />;
 
   return (
     <div className="Music">
-      <button onClick={toggleAudio}>{MusicIcon}</button>
+      <button onClick={toggleAudio}>
+        {" "}
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 24 24"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+          class={isPlaying ? "" : "off"}
+        >
+          <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path>
+        </svg>
+      </button>
       <audio ref={audioRef}>
         <source
           src="https://res.cloudinary.com/dmtn5fbdu/video/upload/v1639103119/skye%2Btyler/Theme_From_New_York_New_York_2008_Remastered.mp3"
