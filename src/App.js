90fb37993937d0adfbb4e7edfed9a929a2718223
Tyler Corbett <tyler.corbett@fast.co>
2021-12-09 18:50:16 -0800

    mvp music

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./reset.css";
 import "./App.scss";
 import TinderSwipe from "./components/TinderSwipe/TinderSwipe";
+import Music from "./components/Music/Music";
 
 const App = () => {
   return (
     <div className="App">
       <TinderSwipe />
+      <Music />
     </div>
   );
 };
---
+++ src/components/Music/Music.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from "react";
+import "./Music.scss";
+import { MdMusicNote as MusicNote } from "react-icons/md";
+
+const Music = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioRef = useRef(null);
+
+  const toggleAudio = () => {
+    if (!isPlaying) {
+      setIsPlaying(true);
+      audioRef.current.play();
+    } else {
+      setIsPlaying(false);
+      audioRef.current.pause();
+    }
+  };
+
+  const MusicIcon = isPlaying ? <MusicNote /> : <MusicNote className="off" />;
+
+  return (
+    <div className="Music">
+      <button onClick={toggleAudio}>{MusicIcon}</button>
+      <audio ref={audioRef}>
+        <source
+          src="https://res.cloudinary.com/dmtn5fbdu/video/upload/v1639103119/skye%2Btyler/Theme_From_New_York_New_York_2008_Remastered.mp3"
+          type="audio/mpeg"
+        />
+      </audio>
+    </div>
+  );
+};
+
+export default Music;
