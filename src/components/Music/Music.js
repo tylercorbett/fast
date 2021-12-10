5c3615afd0e3d6289ea9131c95868c28687eb826
Tyler Corbett <tyler.corbett@fast.co>
2021-12-09 19:34:28 -0800

    try this

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import "./Music.scss";
-// import { MdMusicNote as MusicNote } from "react-icons/md";
+import { MdMusicNote } from "react-icons/md";
 
 const Music = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -17,23 +17,12 @@ const Music = () => {
     }
   };
 
-  // const MusicIcon = isPlaying ? <MusicNote /> : <MusicNote className="off" />;
+  const MusicIcon = isPlaying ? <MdMusicNote /> : <MdMusicNote className="off" />;
 
   return (
     <div className="Music">
       <button onClick={toggleAudio}>
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          stroke-width="0"
-          viewBox="0 0 24 24"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-          class={isPlaying ? "" : "off"}
-        >
-          <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path>
-        </svg>
+        {MusicIcon}
       </button>
       <audio ref={audioRef}>
         <source
