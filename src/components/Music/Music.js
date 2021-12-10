c434651aa851967a7fb9673c4cae1e428cdcc334
Tyler Corbett <tyler.corbett@fast.co>
2021-12-09 19:23:31 -0800

    add some stuff

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -22,7 +22,6 @@ const Music = () => {
   return (
     <div className="Music">
       <button onClick={toggleAudio}>
-        {" "}
         <svg
           stroke="currentColor"
           fill="currentColor"
@@ -31,6 +30,7 @@ const Music = () => {
           height="1em"
           width="1em"
           xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
           class={isPlaying ? "" : "off"}
         >
           <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path>
